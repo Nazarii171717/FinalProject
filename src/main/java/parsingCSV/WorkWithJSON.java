4c26a83a32407ad8f927cef4ab42884700cb6e00
Nazarii171717 <nazarzavgar@gmail.com>
2024-06-13 14:01:21 +0200

    corrected WorkWithJSON

--- src/main/java/parsingCSV/WorkWithJSON.java
+++ src/main/java/parsingCSV/WorkWithJSON.java
@@ -1,34 +1,56 @@
 package parsingCSV;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
 import customer.data.Customer;
 import customer.data.RentalHistory;
 import lombok.SneakyThrows;
 
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Random;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
+import renting.RentalInformation;
 import vehicle.data.Vehicle;
 
-public class WorkWithCSV {
 
-    private final String vehicleDataJSON = "src/main/resources/VehiclesData.json";
+public class WorkWithJSON {
+
+    private final String vehiclesDataJSON = "src/main/resources/VehiclesData.json";
     private final String customersDataJSON = "src/main/resources/CustomersData.json";
     private final String rentalInformationJSON = "src/main/resources/RentalInformation.json";
 
+    private ArrayList<String> allId = new ArrayList<>();
+
+    public String generateUniqueId()
+    {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder id = new StringBuilder();
+        Random rnd = new Random();
+
+        for (int i = 0; i < 5; i++) {
+            id.append(characters.charAt(rnd.nextInt(characters.length())));
+        }
+
+
+        String idf = allId.stream().filter(id1 -> id1.equals(id)).findFirst().orElse(null);
+        if (!idf.equals(null))
+        {
+            id = generateUniqueId();
+        }
+        return String.valueOf(id);
+    }
 
 
     @SneakyThrows
     public Vehicle findVehicleById(String vehicleId)
     {
-        Path path = Path.of(vehicleDataJSON);
+        Path path = Path.of(vehiclesDataJSON);
 
         GsonBuilder gsonBuilder = new GsonBuilder();
         Gson gson = gsonBuilder.create();
@@ -50,20 +72,66 @@ public class WorkWithCSV {
         return vehicle;
     }
 
+    @SneakyThrows
     public Customer findCustomerById(String customerId)
     {
-        return null;
+        Path path = Path.of(customersDataJSON);
+
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        Gson gson = gsonBuilder.create();
+
+        String jsonContent = Files.readString(path);
+        JSONArray jsonArray = new JSONArray(jsonContent);
+
+        ArrayList<Customer> customers = new ArrayList<>();
+        for (Object jsonObject : jsonArray)
+        {
+           Customer customer = gson.fromJson(String.valueOf(jsonObject), Customer.class);
+            customers.add(customer);
+        }
+
+        Customer customer = customers.stream()
+                .filter(customer1 -> customer1.getId().equals(customerId))
+                .findFirst() // id is unique but it requires this function
+                .orElse(null);
+        return customer;
     }
 
-    public void addVehicle(String id, String type, String brand, String model,
+
+    @SneakyThrows
+    public RentalInformation findRentalInformationById(String rentalInformationId)
+    {
+        Path path = Path.of(rentalInformationJSON);
+
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        Gson gson = gsonBuilder.create();
+
+        String jsonContent = Files.readString(path);
+        JSONArray jsonArray = new JSONArray(jsonContent);
+
+        ArrayList<RentalInformation> customers = new ArrayList<>();
+        for (Object jsonObject : jsonArray)
+        {
+            RentalInformation rentalInformation = gson.fromJson(String.valueOf(jsonObject), RentalInformation.class);
+            customers.add(rentalInformation);
+        }
+
+        RentalInformation rentalInformation = customers.stream()
+                .filter(rentalInformation1 -> rentalInformation1.getId().equals(rentalInformationId))
+                .findFirst() // id is unique but it requires this function
+                .orElse(null);
+        return rentalInformation;
+    }
+
+
+
+   /* public void addVehicle(String id, String type, String brand, String model,
      int year, String vin, String color, double maxSpeed, double maxLoad, double engine,
      double fuelTankCapacity, double fuelConsumption, String engineType, String fuelType,
      String gearBox, double mileage, int numberOfSeats, boolean isReserved)
     {
-    }
+    }*/
 
-    //  TODO implement method which write the data about new renting
-    //   to rentalinformation.json following the structure of RentalInformation class
     @SneakyThrows
     public void addRentalInformationRecord(String operationId, String vehicleId, String customerId,
                                            int duration, LocalDateTime rentalDate, LocalDateTime returnDate)
@@ -99,10 +167,6 @@ public class WorkWithCSV {
                             String phone, String email, String address, String id,
                             String password, ArrayList<RentalHistory> rentalHistories, double discount)
     {
-        /*Customer customer = new Customer(name, surname,
-                45, phone,email, address, id, password,
-                new ArrayList<RentalHistory>(), 0.04);
-*/
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("name", name);
         jsonObject.put("surname", surname);
